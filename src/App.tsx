0731858971670c4a948dd5b46c0606cd8167e8de
zain-ali-99 <113171130+zain-ali-99@users.noreply.github.com>
2023-06-01 20:44:46 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,168 @@
+import { CanAccess, GitHubBanner, Refine } from "@refinedev/core";
+import {
+    notificationProvider,
+    ThemedLayoutV2,
+    ErrorComponent,
+    RefineThemes,
+} from "@refinedev/antd";
+import dataProvider from "@refinedev/simple-rest";
+import routerProvider, {
+    NavigateToResource,
+    UnsavedChangesNotifier,
+    DocumentTitleHandler,
+} from "@refinedev/react-router-v6";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import { newEnforcer } from "casbin";
+import { ConfigProvider } from "antd";
+import "@refinedev/antd/dist/reset.css";
+
+import { model, adapter } from "accessControl";
+import { Header } from "components/header";
+import { PostList, PostCreate, PostEdit, PostShow } from "pages/posts";
+import { UserList, UserCreate, UserEdit, UserShow } from "pages/users";
+import {
+    CategoryList,
+    CategoryCreate,
+    CategoryEdit,
+    CategoryShow,
+} from "pages/categories";
+
+const API_URL = "https://api.fake-rest.refine.dev";
+
+const App: React.FC = () => {
+    const role = localStorage.getItem("role") ?? "admin";
+
+    return (
+        <BrowserRouter>
+            <GitHubBanner />
+            <ConfigProvider theme={RefineThemes.Blue}>
+                <Refine
+                    routerProvider={routerProvider}
+                    dataProvider={dataProvider(API_URL)}
+                    accessControlProvider={{
+                        can: async ({ action, params, resource }) => {
+                            const enforcer = await newEnforcer(model, adapter);
+                            if (
+                                action === "delete" ||
+                                action === "edit" ||
+                                action === "show"
+                            ) {
+                                return Promise.resolve({
+                                    can: await enforcer.enforce(
+                                        role,
+                                        `${resource}/${params?.id}`,
+                                        action,
+                                    ),
+                                });
+                            }
+                            if (action === "field") {
+                                return Promise.resolve({
+                                    can: await enforcer.enforce(
+                                        role,
+                                        `${resource}/${params?.field}`,
+                                        action,
+                                    ),
+                                });
+                            }
+                            return {
+                                can: await enforcer.enforce(
+                                    role,
+                                    resource,
+                                    action,
+                                ),
+                            };
+                        },
+                    }}
+                    resources={[
+                        {
+                            name: "posts",
+                            list: "/posts",
+                            show: "/posts/show/:id",
+                            create: "/posts/create",
+                            edit: "/posts/edit/:id",
+                            meta: {
+                                canDelete: true,
+                            },
+                        },
+                        {
+                            name: "users",
+                            list: "/users",
+                            show: "/users/show/:id",
+                            create: "/users/create",
+                            edit: "/users/edit/:id",
+                        },
+                        {
+                            name: "categories",
+                            list: "/categories",
+                            show: "/categories/show/:id",
+                            create: "/categories/create",
+                            edit: "/categories/edit/:id",
+                        },
+                    ]}
+                    notificationProvider={notificationProvider}
+                    options={{
+                        syncWithLocation: true,
+                        warnWhenUnsavedChanges: true,
+                    }}
+                >
+                    <Routes>
+                        <Route
+                            element={
+                                <ThemedLayoutV2
+                                    Header={() => <Header role={role} />}
+                                >
+                                    <CanAccess>
+                                        <Outlet />
+                                    </CanAccess>
+                                </ThemedLayoutV2>
+                            }
+                        >
+                            <Route
+                                index
+                                element={
+                                    <NavigateToResource resource="posts" />
+                                }
+                            />
+
+                            <Route path="/posts">
+                                <Route index element={<PostList />} />
+                                <Route path="create" element={<PostCreate />} />
+                                <Route path="edit/:id" element={<PostEdit />} />
+                                <Route path="show/:id" element={<PostShow />} />
+                            </Route>
+
+                            <Route path="/users">
+                                <Route index element={<UserList />} />
+                                <Route path="create" element={<UserCreate />} />
+                                <Route path="edit/:id" element={<UserEdit />} />
+                                <Route path="show/:id" element={<UserShow />} />
+                            </Route>
+
+                            <Route path="/categories">
+                                <Route index element={<CategoryList />} />
+                                <Route
+                                    path="create"
+                                    element={<CategoryCreate />}
+                                />
+                                <Route
+                                    path="edit/:id"
+                                    element={<CategoryEdit />}
+                                />
+                                <Route
+                                    path="show/:id"
+                                    element={<CategoryShow />}
+                                />
+                            </Route>
+
+                            <Route path="*" element={<ErrorComponent />} />
+                        </Route>
+                    </Routes>
+                    <UnsavedChangesNotifier />
+                    <DocumentTitleHandler />
+                </Refine>
+            </ConfigProvider>
+        </BrowserRouter>
+    );
+};
+
+export default App;
